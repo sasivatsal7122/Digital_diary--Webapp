1f3acf4e90ba0e5f49f9eecf8cf43398a2d8f9bc
SasiVatsal <71326149+sasivatsal7122@users.noreply.github.com>
2022-06-15 21:34:33 +0530

    updated database,main

--- database.py
+++ database.py
@@ -56,10 +56,37 @@ def create_newuser(uNAME,USERNAME,USER_EMAIL,PASS_WORD):
 def create_newpost(USERKEY,USER_POST,C_DATE,C_TIME):
     conn = sqlite3.connect('diary.db')
     cursor = conn.cursor()
+    cursor.execute('SELECT USER_POST FROM USERS_POSTS WHERE USERKEY=(?)',(USERKEY,))
+    user_post = cursor.fetchone()
+    if user_post==None or len(user_post)==0:
+        cursor.execute('INSERT INTO USERS_POSTS(USERKEY, USER_POST, C_DATE,C_TIME) VALUES (?, ?, ?,?)',(USERKEY, USER_POST, C_DATE,C_TIME))
+        conn.commit()
+        conn.close()
+    else:
+        cursor.execute('UPDATE USERS_POSTS SET USER_POST = ? WHERE  USERKEY = ?',(USER_POST,USERKEY))
+        conn.commit()
+        conn.close()
+        
     
-    cursor.execute('INSERT INTO USERS_POSTS(USERKEY, USER_POST, C_DATE,C_TIME) VALUES (?, ?, ?,?)',(USERKEY, USER_POST, C_DATE,C_TIME))
+def get_user_post(userkey):
+    conn = sqlite3.connect('diary.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT USER_POST FROM USERS_POSTS WHERE USERKEY=(?)',(userkey,))
+    user_post = cursor.fetchone()
     conn.commit()
     conn.close()
+    return user_post
+    
+def update_user_post(USERKEY,USER_POST):
+    conn = sqlite3.connect('diary.db')
+    cursor = conn.cursor()
+    cursor.execute('UPDATE USERS_POSTS SET USER_POST = ? WHERE  USERKEY = ?',(USER_POST,USERKEY))
+    conn.commit()
+    conn.close()
+
+    
+    
+    
     
 #create_user_record()
 #create_userposts_record()
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from matplotlib.style import reload_library
 import streamlit as st
 from streamlit_ace import st_ace,THEMES
 import datetime
+from datetime import timedelta
+
+from sympy import use
 import database as db
+from datetime import date
 
 from streamlit_option_menu import option_menu
 
@@ -13,17 +18,82 @@ icons=['box-arrow-in-left', 'box-arrow-in-right'],
 menu_icon="cast", default_index=0, orientation="horizontal")
 st.markdown("<br><br>", unsafe_allow_html=True)
 
-
 def read():
-    pass
+    st.subheader("Read Your Previous data")
+    c1, c2 = st.columns([3, 1])
+    c2.subheader("Parameters")
+   
+    today = date.today()
+    Previous_Date = datetime.datetime.today() - datetime.timedelta(days=1)
+    Previous_Date_Formatted = Previous_Date.strftime ('%Y/%m/%d')
+    
+    user_input_date = c2.date_input(
+     "Choose date",
+     Previous_Date)
+    
+    reload_btn = c2.button("Show Data")
+    if reload_btn:
+    
+        d = user_input_date.strftime('%Y-%m-%d')
+        with open('tempinfo.txt') as f:
+                x = f.read()
+        
+        userkey = str(x)+str(d)
+        user_record_post = db.get_user_post(userkey)
+        
+        try: 
+            
+            with c1:
+                user_post = st_ace(
+                    value=f"{user_record_post[0]}", 
+                    language='plain_text',
+                    theme=c2.selectbox("Theme", options=THEMES, index=35),
+                    font_size=c2.slider("Font size", 5, 24, 14),
+                    tab_size=c2.slider("Tab size", 1, 8, 4),
+                    show_gutter=c2.checkbox("Show gutter", value=True),
+                    wrap=c2.checkbox("Wrap enabled", value=True),
+                    readonly=False,
+                    min_lines=30,
+                    key="ace",
+                    max_lines=None, 
+                    show_print_margin=False, 
+                    annotations=None, 
+                )
+            c2.info("You can edit your data in write section by jumping to the date you want to edit")
 
+        except:
+            pass
+            with c1:
+                user_post = st_ace(
+                    value="You Dont Have any Record on this Date", 
+                    language='plain_text',
+                    theme=c2.selectbox("Theme", options=THEMES, index=35),
+                    font_size=c2.slider("Font size", 5, 24, 14),
+                    tab_size=c2.slider("Tab size", 1, 8, 4),
+                    show_gutter=c2.checkbox("Show gutter", value=True),
+                    wrap=c2.checkbox("Wrap enabled", value=True),
+                    readonly=False,
+                    min_lines=30,
+                    key="ace",
+                    max_lines=None, 
+                    show_print_margin=False, 
+                    annotations=None, 
+                )
+
+        
 def write():
     st.subheader("Write about your day here")
     c1, c2 = st.columns([3, 1])
     c2.subheader("Parameters")
+    today = date.today()
+    current_date = today.strftime('%B %d, %Y')
+    d = c2.date_input(
+     "Choose date",
+     datetime.datetime.now())
+    save_data = st.button("Save Data")
     with c1:
         user_post = st_ace(
-            value='', 
+            value=f"Date: {current_date}", 
             placeholder='Start writing here',
             language='plain_text',
             theme=c2.selectbox("Theme", options=THEMES, index=35),
@@ -33,13 +103,12 @@ def write():
             wrap=c2.checkbox("Wrap enabled", value=True),
             readonly=False,
             min_lines=30,
-            key="ace"
+            key="ace",
+            max_lines=None, 
+            show_print_margin=False, 
+            annotations=None, 
         )
-    d = c2.date_input(
-     "Choose date",
-     datetime.datetime.now())
-    s1,s2,s3 = st.columns(3)
-    save_data = s2.button("Save Data")
+    
     if save_data:
         with open('tempinfo.txt') as f:
             x = f.read()
@@ -49,6 +118,8 @@ def write():
         db.create_newpost(userkey,user_post,d,current_time)           
         st.success("Data Saved Successfully")
             
-if __name__ == "__main__":
+if user_option=='Write MY Diary':
     write()
+else:
+    read()
 
